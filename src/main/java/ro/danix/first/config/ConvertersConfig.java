1847e89faed57911d2e3a0c79edc0be81ca73866
dpersa <dpersa@groupon.com>
2013-01-18 18:07:08 +0100

    started to fix the controllers

---
+++ src/main/java/ro/danix/first/config/ConvertersConfig.java
@@ -0,0 +1,31 @@
+package ro.danix.first.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+
+/**
+ *
+ * @author danix
+ */
+@Configuration
+public class ConvertersConfig {
+
+
+    @Bean
+    public MappingJacksonHttpMessageConverter mappingJacksonHttpMessageConverter() {
+        return new MappingJacksonHttpMessageConverter();
+    }
+
+    @Bean
+    public StringHttpMessageConverter stringHttpMessageConverter() {
+        return new StringHttpMessageConverter();
+    }
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter() {
+        return new ByteArrayHttpMessageConverter();
+    }
+}
