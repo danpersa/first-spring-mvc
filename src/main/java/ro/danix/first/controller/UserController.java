3da7e76b7e1468831cbaa36323f38eb064f92dd6
dpersa <dpersa@groupon.com>
2013-01-21 18:35:03 +0100

    removed methods not related to the API

--- src/main/java/ro/danix/first/controller/UserController.java
+++ src/main/java/ro/danix/first/controller/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import ro.danix.first.model.domain.EmailAddress;
 import ro.danix.first.model.domain.user.User;
 import ro.danix.first.model.service.user.UserService;
 
@@ -26,65 +25,28 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String index(Model model) {
-        model.addAttribute("users", userService.findAll());
-        return "users/index";
-    }
-
     @RequestMapping(value = "/json", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
-    List<User> indexJson() {
+    List<User> index() {
         List<User> users = userService.findAll();
         return users;
-    }
-    
-    @RequestMapping(value = "/js", method = RequestMethod.GET, produces = "text/javascript")
-    public String indexJs() {
-        List<User> users = userService.findAll();
-        return "users/index";
-    }
-
-    @RequestMapping(value = "/new", method = RequestMethod.GET)
-    public String newForm(Model model) {
-        model.addAttribute(new User("danix", new EmailAddress("danix@yahoo.com")));
-        return "users/new";
-    }
-
-    // The user is created with its default constructor.
-    // Data binding and validation are also applied.
-    @RequestMapping(method = RequestMethod.POST)
-    public String create(@Valid @ModelAttribute User user, BindingResult result) {
-        if (result.hasErrors()) {
-            return "users/edit";
-        }
-        this.userService.save(user);
-        return "redirect:users";
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public @ResponseBody
-    User create(@RequestBody User user) {
+    User create(@Valid @ModelAttribute User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return null;
+        }
         userService.save(user);
         return user;
     }
 
     // The user is loaded from the "user" URI variable via {@link UserConverter}
-    @RequestMapping(value = "/{user}", method = RequestMethod.GET)
-    public String show(@PathVariable User user) {
-        return "users/show";
-    }
-
     @RequestMapping(value = "/{user}/json", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
-    User showJson(@PathVariable User user) {
+    User show(@PathVariable User user) {
         return user;
-    }
-
-    // The user is loaded from the "user" URI variable via {@link UserConverter}
-    @RequestMapping(value = "/{user}/edit", method = RequestMethod.GET)
-    public String edit(@PathVariable User user) {
-        return "users/edit";
     }
 
     // The user is loaded from the "user" URI variable via {@link UserConverter}.
