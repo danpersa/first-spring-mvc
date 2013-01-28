7e9a30bc4bb468cc460f7b9e0fdbbad8c465d6c2
dpersa <dpersa@groupon.com>
2013-01-28 10:07:34 +0100

    Added create test

--- src/test/java/ro/danix/first/controller/UserControllerTest.java
+++ src/test/java/ro/danix/first/controller/UserControllerTest.java
@@ -1,10 +1,11 @@
 package ro.danix.first.controller;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.hamcrest.Matchers.*;
 import java.math.BigInteger;
 import org.junit.*;
 import org.junit.experimental.categories.Category;
@@ -15,6 +16,7 @@ import ro.danix.first.model.domain.user.User;
 import ro.danix.first.model.domain.user.factory.UserFactory;
 import ro.danix.first.model.service.user.UserService;
 import ro.danix.test.FastRunningTests;
+import ro.danix.test.IntegrationTestUtil;
 
 /**
  *
@@ -46,14 +48,27 @@ public class UserControllerTest {
     }
 
     @Test
-    public void shouldGetTestUserAsJson() throws Exception {
+    public void showTest() throws Exception {
         mockMvc
                 .perform(get("/users/0")
                 .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("username", is(UserFactory.USERNAME)))
                 .andExpect(jsonPath("lastname", is(UserFactory.LAST_NAME)));
+    }
+
+    @Test
+    public void createTest() throws Exception {
+        User user = userFactory.build();
+        mockMvc.perform(post("/users")
+                .contentType(IntegrationTestUtil.APPLICATION_JSON_UTF8)
+                .content(IntegrationTestUtil.convertObjectToJsonBytes(user)))
+                .andExpect(status().isBadRequest())
+                .andDo(print())
+                .andExpect(content().contentType(IntegrationTestUtil.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(startsWith("{\"fieldErrors\":[")));
+//                .andExpect(content().mimeType(IntegrationTestUtil.APPLICATION_JSON_UTF8))
+//                .andExpect(content().string("{\"fieldErrors\":[{\"path\":\"title\",\"message\":\"The title cannot be empty.\"}]}"));
     }
 }
---
+++ src/test/java/ro/danix/test/IntegrationTestUtil.java
@@ -0,0 +1,29 @@
+package ro.danix.test;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author dpersa
+ */
+public class IntegrationTestUtil {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+        return mapper.writeValueAsBytes(object);
+    }
+    
+    public static String convertObjectToJson(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
+        return mapper.writeValueAsString(object);
+    }
+}
